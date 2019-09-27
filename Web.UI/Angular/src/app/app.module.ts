02b7f8a65636e50434743c4f01e1fc689611ee77
egork <egork@mail.ru>
2019-09-27 04:05:02 -0700

    + Angualr

---
+++ Web.UI/Angular/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/* material module */
+import { MaterialModule } from './material.module';
+
+import { DataTableViewComponent } from '../components/data.table.view';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataTableViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
